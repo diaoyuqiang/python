579619e6789ea5921a0cac9136429e84156dd80d
diao-yuqiang <1948502581@qq.com>
2022-05-18 16:33:43 +0800

    dyq

--- deque模块（双向队列）.py
+++ deque模块（双向队列）.py
@@ -7,11 +7,16 @@ from collections import deque
 # q.appendleft() # 队首进队
 # q.pop() # 队尾出队
 
-def tail(n):
-    with open('text.txt', 'r') as f:
-        q = deque(f, n)
-        return q
+# def tail(n):
+#     with open('text.txt', 'r') as f:
+#         q = deque(f, n)
+#         return q
+#
+# for line in tail(5):
+#     print(line, end="")
 
-for line in tail(5):
-    print(line, end="")
-
+q1 = deque(maxlen=2)  # 从右边添加，满了从左边剔除
+q1.append(1)
+q1.append(2)
+q1.append(3)
+print(q1)
