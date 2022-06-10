01d7d723d69e50ffaafed98dedd8e0c592bbfe3a
diao-yuqiang <1948502581@qq.com>
2022-06-10 16:27:27 +0800

    dyq

--- python注解.py
+++ python注解.py
@@ -3,7 +3,7 @@ def demo(num:int, sr:str, n:'int>9'=10)->bool:
     print(num, sr, n)
     return True
 
-demo(1, "dyq")
+demo(1, "dyq", 8)
 
 # TODO admin:dyq 高亮注释，可在project中查询
 # 打印函数注解
---
+++ python注释.py
@@ -0,0 +1,5 @@
+import numpy  # noqa: F401 表示忽略导入未使用
+
+def writepython():  # noqa  表示忽略ide检错
+    print("no quality assurance")
+    return 5  # type: dict  # 用于指定期望的数据类型
